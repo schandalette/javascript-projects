c6864e926d8fc53fc8d32c71179bbba4cc7da520
schandalette <schanda.lynn@gmail.com>
2024-08-06 16:28:16 -0500

    DOM exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,24 @@
-function init () {
+function init() {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    })
+    missionAbort.addEventListener('mouseover', () => {
+        missionAbort.style.backgroundColor = 'red';
+    })
+    missionAbort.addEventListener('mouseout', () => {
+        missionAbort.style.backgroundColor = '';
+    })
+    missionAbort.addEventListener('click', event => {
+        const confirmAbort = confirm("Are you sure you want to abort the mission?")
+        if(confirmAbort) {
+          paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.';
+        }
+    })
 }
 
 window.addEventListener("load", init);
